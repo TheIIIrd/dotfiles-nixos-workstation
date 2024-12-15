c619f11c3968c4d4e657735179fc800af9a40b18
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-12-15 18:05:20 +0300

    Fixes after additional tests

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -13,9 +13,9 @@
         nh-upd = "nh os switch --update";
         nh-hms = "nh home switch";
 
-        nix-rb = "sudo nixos-rebuild switch --flake ${flakeDir}";
-        nix-upd = "nix flake update --flake ${flakeDir}";
-        nix-upg = "sudo nixos-rebuild boot --upgrade --flake ${flakeDir}";
+        # nix-rb = "sudo nixos-rebuild switch --flake ${flakeDir}";
+        # nix-upd = "nix flake update --flake ${flakeDir}";
+        # nix-upg = "sudo nixos-rebuild boot --upgrade --flake ${flakeDir}";
 
         pkgs = "nvim ${flakeDir}/nixos/packages.nix";
 
